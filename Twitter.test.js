02ca29f212cad109cdaf089632007f64ce0f2b22
klitoschr <42859502+klitoschr@users.noreply.github.com>
2018-11-18 08:39:08 +0200

    Add files via upload

--- Twitter.test.js
+++ Twitter.test.js
@@ -16,8 +16,9 @@ beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
-  // Use one of those accounts to deploy
-  // the contract
+  /* Use one of those accounts to deploy the contract each time work 
+  * with a fresh copy of the deployed contract.
+  */
   twitter = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
@@ -26,13 +27,15 @@ beforeEach(async () => {
     .send({ from: accounts[0], gas: '1000000' });
 });
 
-//Testing using some assertions  
+//Testing using some assertions
+// it statements meant to test a specific part of the contract
 describe('Twitter', () => {
   it('deploys a contract', () => {
     assert.ok(twitter.options.address);
   });
 
   it('has a default message', async () => {
+  	//get access to a specific method of the contract
     const message = await twitter.methods.message().call();
     assert.equal(message, 'My tweet!');
   });
